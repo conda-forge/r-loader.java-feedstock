4e8b03300b96ca38ab9166c54a117094b38730dd
mor427unican <mor427@alumnos.unican.es>
2025-09-22 19:25:16 +0200

    NetCDF-java dependence and activate and deactivate scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d
+# This will allow them to be run on environment activation
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done

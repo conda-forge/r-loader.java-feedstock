da31ae1efbe28e4c5570f6efa2417dbf1308e56c
mor427unican <mor427@alumnos.unican.es>
2025-10-02 18:39:26 +0200

    Include .bat scripts in build.sh, drop bld.bat, relax netcdf-java dependency

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,4 +8,5 @@ for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
     cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.bat" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.bat"
 done
